7a61d7e60bfa49681870523f857ecb51cec196ac
Amirmeyo <166917508+Amirmeyo@users.noreply.github.com>
2026-08-12 08:06:11 +0330

    Create Color.kt

---
+++ app/src/main/java/com/dnsir/app/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.dnsir.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NeonPurple = Color(0xFF8A2BE2)
+val NeonCyan = Color(0xFF00E5FF)
+val DarkBg = Color(0xFF0D0D14)
+val DarkSurface = Color(0xFF16161F)
+val AccentGreen = Color(0xFF00FFA3)
+val TextPrimary = Color(0xFFF2F2F7)
+val TextSecondary = Color(0xFFA0A0AE)
